8c2566b680938f6c7776df0da443186ac81ef930
Kamiel Brees <kamiel.brees@gmail.com>
2022-06-06 20:13:06 +0200

    changed offsets

--- Offsets.h
+++ Offsets.h
@@ -4,26 +4,26 @@
 
 
 namespace offsets {
-	constexpr int Entitylist = 0x4DD69DC;
-	constexpr int RadarBase = 0x520B754;
+	constexpr int Entitylist = 0x4DD7B1C;
+	constexpr int RadarBase = 0x520C894;
 	constexpr int Health = 0x100;
 	constexpr int VecOrigin = 0x138;
 	constexpr int bDormant = 0xED;
 	constexpr int ViewAngles = 0x4D90;
 	constexpr int ClientState = 0x58BFDC;
-	constexpr int LocalPlayer = 0xDBA5BC;
+	constexpr int LocalPlayer = 0xDBB5CC;
 	constexpr int TeamNum = 0xF4;
 	constexpr int VecView = 0x108;
 	constexpr int BoneMatrix = 0x26A8;
 	constexpr int GlowIndex = 0x10488;
-	constexpr int GlowObjectManager = 0x531F608;
+	constexpr int GlowObjectManager = 0x5320758;
 	constexpr int GetLocalPlayer = 0x180;
 	constexpr int SpottedByMask = 0x980;
 	constexpr int Flag = 0x104;
-	constexpr int ForceJump = 0x5280924;
-	constexpr int ForceAttack = 0x3206E9C;
+	constexpr int ForceJump = 0x5281A64;
+	constexpr int ForceAttack = 0x3207FDC;
 	constexpr int CrosshairId = 0x11838;
-	constexpr int ViewMatrix = 0x4DC82F4;
+	constexpr int ViewMatrix = 0x4DC9434;
 }
 	
 
